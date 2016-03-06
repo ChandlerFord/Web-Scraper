8697c9f1496647721b0ce7449475e1003a36adef
ChandlerFord <ChandlerFord452@gmail.com>
2016-03-06 02:25:38 -0500

    Test 1
    Attempt to read table info from site, but beautifulsoup does not find
    the table and store its info to table variable...

--- Web Scraper/Scraper.py
+++ Web Scraper/Scraper.py
@@ -1,9 +1,13 @@
 __author__ = 'Chandler'
 
 import requests
-import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 
 url= 'http://www.pcmag.com/article2/0,2817,2369981,00.asp'
 response = requests.get(url)
 html = response.content
-print html
+
+soup = BeautifulSoup(html)
+table = soup.find('tbody', attrs={'class': 'stripe'})
+print table.prettify()
+#print soup.prettify()
