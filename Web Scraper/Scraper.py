75635ecd7db7efe4700763a821868e7ffd4415b9
ChandlerFord <ChandlerFord452@gmail.com>
2016-03-07 14:29:54 -0500

    Web-Scraper (Inmates)
    Finished tutorial of "First web scraper"
    -output file "inmates.csv" attached

--- Web Scraper/Scraper.py
+++ Web Scraper/Scraper.py
@@ -1,5 +1,6 @@
 __author__ = 'Chandler'
 
+import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
@@ -8,6 +9,20 @@ response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-#print soup.prettify()
 table = soup.find('tbody', attrs={'class': 'stripe'})
-print table.prettify()
+
+list_of_rows = []
+for row in table.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        text = cell.text
+        list_of_cells.append(text)
+    #print list_of_cells (each persons info printed on new line)
+    #Adds the cells to 1 row
+    list_of_rows.append(list_of_cells)
+#print list_of_rows (Prints out all info on one line)
+
+#Output info to .csv file
+outfile = open("./inmates.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerows(list_of_rows)
