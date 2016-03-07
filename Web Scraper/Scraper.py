a66668f08aec554b8e35f881a7b126df03637f16
ChandlerFord <ChandlerFord452@gmail.com>
2016-03-07 14:14:47 -0500

    Test 1 revised
    Original code works, but only with example website for learning
    purposes.
    
    -Program outputs unformatted table info

--- Web Scraper/Scraper.py
+++ Web Scraper/Scraper.py
@@ -3,11 +3,11 @@ __author__ = 'Chandler'
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url= 'http://www.pcmag.com/article2/0,2817,2369981,00.asp'
+url= 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s'
 response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
+#print soup.prettify()
 table = soup.find('tbody', attrs={'class': 'stripe'})
 print table.prettify()
-#print soup.prettify()
