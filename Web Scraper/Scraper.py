9b9a9000be692669d0ef016b80e4fe3b57469593
ChandlerFord <ChandlerFord452@gmail.com>
2016-03-07 15:10:21 -0500

    Website of choice scrape
    Custom Web-scraper designed to work with the URL provided.
    -Had to change container name and class to find table data
    -Outputs data to "laptopcompare.csv" hard to read to human eye, but all
    values separated by commas (csv)

--- Web Scraper/Scraper.py
+++ Web Scraper/Scraper.py
@@ -4,25 +4,23 @@ import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url= 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s'
+url= 'http://www.pcmag.com/article2/0,2817,2369981,00.asp'
 response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-table = soup.find('tbody', attrs={'class': 'stripe'})
+table = soup.find('table', attrs={'class': 'roundup-chart'})
 
 list_of_rows = []
 for row in table.findAll('tr'):
     list_of_cells = []
     for cell in row.findAll('td'):
-        text = cell.text
+        text = cell.text.replace('&nbsp;', '')
         list_of_cells.append(text)
-    #print list_of_cells (each persons info printed on new line)
-    #Adds the cells to 1 row
     list_of_rows.append(list_of_cells)
-#print list_of_rows (Prints out all info on one line)
+print list_of_rows
 
 #Output info to .csv file
-outfile = open("./inmates.csv", "wb")
+outfile = open("./laptopcompare.csv", "wb")
 writer = csv.writer(outfile)
 writer.writerows(list_of_rows)
